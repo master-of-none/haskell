d5352b8c5832bf5643d9b114ef607f1a992bb880
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-10-21 11:53:37 -0700

    functions

--- types/type.hs
+++ types/type.hs
@@ -6,3 +6,20 @@
 -}
 x :: Int
 x = 3
+
+-- The below Code yields infinite loop
+{-
+y :: Int
+y = y + 1
+-}
+
+-- Functions
+sumtorial :: Integer -> Integer
+sumtorial 0 = 0
+sumtorial n = n + sumtorial (n - 1)
+
+-- Can also write the above function as below
+sums :: Integer -> Integer
+sums n
+  | n == 0 = 0
+  | otherwise = n + sums (n - 1)
