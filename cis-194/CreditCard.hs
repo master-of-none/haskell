fa8a004b8a1b12d51ddb93bad2ab3719a7d8e750
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-11-05 22:39:30 -0800

    Credit Card exercise 1 - part1

---
+++ cis-194/CreditCard.hs
@@ -0,0 +1,17 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module CreditCard where
+
+toDigits :: Integer -> [Integer]
+toDigits n
+  | n <= 0 = []
+  | otherwise = toDigits (n `div` 10) ++ [n `mod` 10]
+
+toDigitsTest :: Bool
+toDigitsTest = and
+ [
+  [1] == toDigits 1,
+  [1, 2, 3, 4] == toDigits 1234,
+  null (toDigits 0),
+  null (toDigits (-17))
+ ]
