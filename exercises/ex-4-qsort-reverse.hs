5a6253c53c034cdaee04e4c3ac22ef709d4bee72
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-11-17 16:56:44 -0800

    Added Haskell MOOC exercises

--- exercises/ex-4-qsort-reverse.hs
+++
@@ -1,6 +0,0 @@
-qsortReverse :: [Int] -> [Int]
-qsortReverse [] = []
-qsortReverse (x : xs) = qsortReverse larger ++ [x] ++ qsortReverse smaller
-  where
-    smaller = [a | a <- xs, a <= x]
-    larger = [b | b <- xs, b > x]
